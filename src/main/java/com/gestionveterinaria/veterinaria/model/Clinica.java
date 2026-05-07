20da2d1b999851f54e58313dc7924bde70b4ea12
palomvmoreno <pamola0720@gmail.com>
2026-05-07 14:19:06 -0400

    Model Clinica Actulizado

--- src/main/java/com/gestionveterinaria/veterinaria/model/Clinica.java
+++ src/main/java/com/gestionveterinaria/veterinaria/model/Clinica.java
@@ -1,5 +1,40 @@
 package com.gestionveterinaria.veterinaria.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "clinicas")
 public class Clinica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre de la clínica es obligatorio")
+    private String nombre;
+
+    @NotBlank(message = "La dirección es obligatoria")
+    @Column(nullable = false)
+    private String direccion;
+
+    @ManyToOne
+    @JoinColumn(name = "comuna_id")
+    private Comuna comuna;
 
 }
