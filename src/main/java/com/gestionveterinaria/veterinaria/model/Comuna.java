13dcd6c46fec983eb06c0f4e267f27f25bfb13aa
palomvmoreno <pamola0720@gmail.com>
2026-05-07 13:39:44 -0400

    Creacion Model Comuna

--- src/main/java/com/gestionveterinaria/veterinaria/model/Comuna.java
+++ src/main/java/com/gestionveterinaria/veterinaria/model/Comuna.java
@@ -1,5 +1,42 @@
 package com.gestionveterinaria.veterinaria.model;
 
-public class Comuna {
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comunas")
+public class Comuna {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre de la comuna es obligatorio")
+    @Size(min = 2, max = 30, message = "El nombre debe tener entre 2 y 30 caracteres")
+    @Column(nullable = false, length = 30)
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "region_id")
+    private Region region;
+    
+    
 }
