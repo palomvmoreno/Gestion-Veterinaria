c60507a22a9b0fe1c24e83c61539d0e069173034
palomvmoreno <pamola0720@gmail.com>
2026-05-08 12:01:26 -0400

    Creacion Model Veterinario

---
+++ src/main/java/com/gestionveterinaria/veterinaria/model/Veterinario.java
@@ -0,0 +1,41 @@
+package com.gestionveterinaria.veterinaria.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "veterinario")
+public class Veterinario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 3, max = 50, message = "El nombre debe tener entre 3 y 50 caracteres")
+    @Column(nullable = false, length = 50)
+    private String nombre;
+
+    @NotBlank(message = "El apellido es obligatorio")
+    @Size(min = 3, max = 50, message = "El apellido debe tener entre 3 y 50 caracteres")
+    @Column(nullable = false, length = 50)
+    private String apellido;
+
+    @NotBlank(message = "La especialidad es obligatoria")
+    @Column(nullable = false, length = 50)
+    private String especialidad;
+
+}
