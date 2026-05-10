36c902144a1857f14fc65b323b3d2760a6e3e1c6
palomvmoreno <pamola0720@gmail.com>
2026-05-09 22:08:37 -0400

    correcion

--- src/main/java/com/gestionveterinaria/veterinaria/model/Veterinarios.java
+++ src/main/java/com/gestionveterinaria/veterinaria/model/Veterinarios.java
@@ -7,12 +7,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "veterinarios")
